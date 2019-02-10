6b536ce873f107b83bc5ac5ab55a1e2d68ecd04d
Unknown <yoann.legrand@etud.u-picardie.fr>
2019-02-10 18:15:57 +0100

    Initialisation du plateau et ajout de pions
    Ajout de pion lors du clic

--- src/gui/controllers/GameController.java
+++ src/gui/controllers/GameController.java
@@ -2,36 +2,68 @@ package gui.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Cylinder;
+import javafx.scene.transform.Rotate;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import static javafx.scene.control.ContentDisplay.CENTER;
 
 public class GameController implements Initializable {
 
     @FXML
     Label labelJoueur1;
 
-    public void setLabelJoueur1(String labelJoueur1) {
-        this.labelJoueur1.setText(labelJoueur1);
-    }
+    @FXML
+    GridPane gridPaneGame;
 
-    public void setJoueur1(String joueur1) {
-        this.joueur1 = joueur1;
-    }
-
-    public void setJoueur2(String joueur2) {
-        this.joueur2 = joueur2;
-    }
-
-    String joueur1, joueur2;
+   private Image black, white, empty;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        black = new Image("img/blackDisk.png");
+        white = new Image("img/whiteDisk.png");
+        empty = new Image("img/emptyDisk.png");
+        for (int i = 0; i < 8; i++){ //Per righe
+            for (int j = 0; j < 8; j++){ // Per colonne
 
-        System.out.println("joueur1" + joueur1);
-        System.out.println("avant" +labelJoueur1.getText());
-        labelJoueur1.setText(joueur1);
-        System.out.println("apres" + labelJoueur1.getText());
+                gridPaneGame.add(new ImageView(empty), i, j);
+            }
+        }
+        gridPaneGame.add(new ImageView(white), 3, 3);
+        gridPaneGame.add(new ImageView(black), 3, 4);
+        gridPaneGame.add(new ImageView(black), 4, 3);
+        gridPaneGame.add(new ImageView(white), 4, 4);
+
     }
+
+    @FXML
+    private void clickGrid(MouseEvent event) {
+        //black = new Image("img/test.png");
+        //white = new Image("img/test.png");
+        //empty = new Image("img/test.png");
+
+        Node source = (Node)event.getTarget() ;
+        Integer colIndex = GridPane.getColumnIndex(source);
+        Integer rowIndex = GridPane.getRowIndex(source);
+        System.out.printf("Mouse clicked cell [%d, %d]%n", colIndex, rowIndex);
+
+        gridPaneGame.add(new ImageView(white), colIndex, rowIndex);
+
+    }
+
 }
